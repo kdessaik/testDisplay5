5c9171d129e2cdd71b6a27f56d30decc62dd1362
Kambale Kibeho <dessaikibeho@gmail.com>
2022-06-06 12:25:36 +0300

    jkk

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <p> My name is Kambale Kibeho dessai I am a congolese by nationa</p>
         </a>
       </header>
     </div>
